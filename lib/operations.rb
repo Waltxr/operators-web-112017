17423ae0960e887ba035a675926daabaf1fafcb8
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 17:02:05 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,5 @@
 def unsafe?(speed)
-	if speed < 40
-		return true
-	elsif speed > 60
+	if speed < 40 || speed > 60
 		return true
 	else
 		return false
@@ -11,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-
+	
 end
